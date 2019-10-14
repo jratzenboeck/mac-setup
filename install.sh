1df09c0899876fc82ed53d5aa4fdc2459e10363d
Jürgen Ratzenböck <juergen.ratzenboeck@gmail.com>
2019-10-14 20:41:17 +0200

    Enable chsh + install oh-my-zsh + zsh-nvm

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@
 
 echo "Setting up your Mac..."
 
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Install Oh My Zsh if it isn't already present
+if [[ ! -d $HOME/oh-my-zsh/ ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# ---------- Install custom oh-my-zsh plugins ----------- #
+# NVM
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -13,15 +25,6 @@ brew update
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle
-
-# Make ZSH the default shell environment
-# chsh -s $(which zsh)
-
-## TODO: Comment in after trying it with first setup
-# Install Oh My Zsh if it isn't already present
-# if [[ ! -d $HOME/oh-my-zsh/ ]]; then
-#  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# fi
 
 # Create a Sites directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
